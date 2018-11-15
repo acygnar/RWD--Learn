a460a726ba1302b429789e65d8ad3220a6db31da
acygnar <42324874+acygnar@users.noreply.github.com>
2018-11-15 19:23:41 +0100

    Add files via upload

---
+++ Responsive/script/js-script.js
@@ -0,0 +1,37 @@
+(function(){
+    $(window).scroll(function(e) {
+            var currentposition = $(document).scrollTop();
+            var bannerheight = $(".main-nav").height();
+            if (currentposition > bannerheight) {
+                $("header").addClass("opacity");
+            } else {
+                $("header").removeClass("opacity");
+            }
+        });
+    
+})();
+
+var images = [
+                'img/views1.jpg',
+                'img/views2.jpg',
+                'img/views3.jpg'
+            ];
+            var num = 0;
+
+            function next() {
+                var slider = document.getElementById('slide');
+                num++;
+                if (num >= images.length) {
+                    num = 0;
+                }
+                slider.src = images[num];
+            }
+
+            function prev() {
+                var slider = document.getElementById('slide');
+                num--;
+                if (num < 0) {
+                    num = images.length - 1;
+                }
+                slider.src = images[num];
+            }
