a2ed0010b626523bce79e55d521cc7cc9aa676e9
acygnar <42324874+acygnar@users.noreply.github.com>
2018-11-25 21:56:47 +0100

    Add files via upload

--- Responsive/script/js-script.js
+++ Responsive/script/js-script.js
@@ -1,37 +1,36 @@
-(function(){
-    $(window).scroll(function(e) {
-            var currentposition = $(document).scrollTop();
-            var bannerheight = $(".main-nav").height();
-            if (currentposition > bannerheight) {
-                $("header").addClass("opacity");
-            } else {
-                $("header").removeClass("opacity");
-            }
-        });
-    
-})();
+(function () {
+    $(window).scroll(function (e) {
+        var currentposition = $(document).scrollTop();
+        var bannerheight = $(".main-nav").height();
+        if (currentposition > bannerheight) {
+            $("header").addClass("opacity");
+        } else {
+            $("header").removeClass("opacity");
+        }
+    });
 
+})();
 var images = [
                 'img/views1.jpg',
                 'img/views2.jpg',
-                'img/views3.jpg'
+                'img/views3.jpg',
             ];
-            var num = 0;
+var num = 0;
 
-            function next() {
-                var slider = document.getElementById('slide');
-                num++;
-                if (num >= images.length) {
-                    num = 0;
-                }
-                slider.src = images[num];
-            }
+function next() {
+    var slider = document.querySelector('.slide');
+    num++;
+    if (num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
 
-            function prev() {
-                var slider = document.getElementById('slide');
-                num--;
-                if (num < 0) {
-                    num = images.length - 1;
-                }
-                slider.src = images[num];
-            }
+function prev() {
+    var slider = document.querySelector('.slide');
+    num--;
+    if (num < 0) {
+        num = images.length - 1;
+    }
+    slider.src = images[num];
+}
