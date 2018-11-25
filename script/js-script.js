f96c355903f5506e18d6266e2d8a284bc8b62d87
acygnar <42324874+acygnar@users.noreply.github.com>
2018-11-25 21:57:30 +0100

    Add files via upload

--- script/js-script.js
+++ script/js-script.js
@@ -1,55 +1,36 @@
-(function(){
-    $(window).scroll(function(e) {
-            var currentposition = $(document).scrollTop();
-            var bannerheight = $(".main-nav").height();
-            if (currentposition > bannerheight) {
-                $("header").addClass("opacity");
-            } else {
-                $("header").removeClass("opacity");
-            }
-        });
-    
+(function () {
+    $(window).scroll(function (e) {
+        var currentposition = $(document).scrollTop();
+        var bannerheight = $(".main-nav").height();
+        if (currentposition > bannerheight) {
+            $("header").addClass("opacity");
+        } else {
+            $("header").removeClass("opacity");
+        }
+    });
+
 })();
-    
-
-function openNav(){
-    
-    document.querySelector('.slide-nav').style.width= "200px";
-    const button=document.querySelector('.burger');
-    
-    button.addEventListener('click',openNav,false);
-    button.addEventListener('click',closeNav,false); 
-   }
-
-function closeNav(){
-    document.querySelector('.slide-nav').style.width= "0";
-}
-
-
-
-
-
 var images = [
                 'img/views1.jpg',
                 'img/views2.jpg',
-                'img/views3.jpg'
+                'img/views3.jpg',
             ];
-            var num = 0;
+var num = 0;
 
-            function next() {
-                var slider = document.querySelector('.slide');
-                num++;
-                if (num >= images.length) {
-                    num = 0;
-                }
-                slider.src = images[num];
-            }
+function next() {
+    var slider = document.querySelector('.slide');
+    num++;
+    if (num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
 
-            function prev() {
-                var slider = document.querySelector('.slide');
-                num--;
-                if (num < 0) {
-                    num = images.length - 1;
-                }
-                slider.src = images[num];
-            }
+function prev() {
+    var slider = document.querySelector('.slide');
+    num--;
+    if (num < 0) {
+        num = images.length - 1;
+    }
+    slider.src = images[num];
+}
